3cce07f8bf59ecf6ece792dddbe9676745ff4aa3
rodolpop <garciasena@yahoo.com>
2023-05-24 15:58:20 +0200

    WeatherApp

---
+++ src/components/weatherApp.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import WeatherForm from "./weatherForm";
+import WeatherMainInfo from "./weatherMainInfo";
+
+export default function WeatherApp(){
+    const [weather, setWeather] = useState(null);
+
+    useEffect(() => {
+
+    }, []);
+
+    async function loadInfo( city = 'london'){
+
+        try{
+            const request = await fetch( 
+
+                //`${process.env.REACT_APP_URL}&key=${process.env.REACT_APP_KEY}&q=${city}`
+                //`${process.env.VITE_APP_URL}&key=${process.env.VITE_APP_KEY}&q=${city}`
+                `${import.meta.env.VITE_APP_URL}&key=${import.meta.env.VITE_APP_KEY}&q=${city}`
+                
+                );
+
+                const json = await request.json();
+
+                setWeather(json);
+
+                console.log(json);
+
+            
+
+
+                setTimeout(() => {
+                    setWeather(json);
+                }, 2000);
+
+        } catch ( error ) {}
+
+    }
+
+    function handleChangeCity(city){
+        setWeather(null);
+        loadInfo(city);
+    }
+
+    return (
+        
+        <div>
+
+            <WeatherForm onChangeCity={handleChangeCity} />
+            <WeatherMainInfo weather={weather} />
+            
+            <div></div>
+
+        </div>
+        
+        );
+}
